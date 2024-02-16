07528d73ea893bbdd3288c07716b6401568f5c51
harshchahande <harshchahande123@gmail.com>
2024-02-16 19:24:39 +0530

    Initial commit

---
+++ js.js
@@ -0,0 +1,78 @@
+var a = document.getElementById("s5-j");
+var b = document.getElementById("s5-j1");
+var b1 = document.getElementById("s5-b");
+function s() {
+  a.style.backgroundColor = " #08bf2d";
+  b.style.color = "white";
+  b1.style.borderColor = " #08bf2d";
+}
+function s1() {
+  a.style.backgroundColor = "white";
+  b.style.color = "#08bf2d";
+  b1.style.borderColor = "white";
+}
+var c = document.getElementById("s5-j2");
+var d = document.getElementById("s5-j3");
+var b2 = document.getElementById("s5-b1");
+function s2() {
+  c.style.backgroundColor = " #08bf2d";
+  d.style.color = "white";
+  b2.style.borderColor = " #08bf2d";
+}
+function s3() {
+  c.style.backgroundColor = "white";
+  d.style.color = "#08bf2d";
+  b2.style.borderColor = "white";
+}
+var e = document.getElementById("s5-j4");
+var f = document.getElementById("s5-j5");
+var b3 = document.getElementById("s5-b2");
+function s4() {
+  e.style.backgroundColor = " #08bf2d";
+  f.style.color = "white";
+  b3.style.borderColor = " #08bf2d";
+}
+function s5() {
+  e.style.backgroundColor = "white";
+  f.style.color = "#08bf2d";
+  b3.style.borderColor = " white";
+}
+var g = document.getElementById("s5-j6");
+var h = document.getElementById("s5-j7");
+var b4 = document.getElementById("s5-b3");
+function s6() {
+  g.style.backgroundColor = " #08bf2d";
+  h.style.color = "white";
+  b4.style.borderColor = " #08bf2d";
+}
+function s7() {
+  g.style.backgroundColor = "white";
+  h.style.color = "#08bf2d";
+  b4.style.borderColor = " white";
+}
+var i = document.getElementById("s5-j8");
+var j = document.getElementById("s5-j9");
+var b5 = document.getElementById("s5-b4");
+function s8() {
+  i.style.backgroundColor = " #08bf2d";
+  j.style.color = "white";
+  b5.style.borderColor = " #08bf2d";
+}
+function s9() {
+  i.style.backgroundColor = "white";
+  j.style.color = "#08bf2d";
+  b5.style.borderColor = " white";
+}
+var k = document.getElementById("s5-j10");
+var l = document.getElementById("s5-j11");
+var b6 = document.getElementById("s5-b5");
+function s10() {
+  b6.style.borderColor = " #08bf2d";
+  k.style.backgroundColor = " #08bf2d";
+  l.style.color = "white";
+}
+function s11() {
+  k.style.backgroundColor = "white";
+  l.style.color = "#08bf2d";
+  b6.style.borderColor = " white";
+}
